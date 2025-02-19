ca12bff77c162e83f35422126e5603a15c793363
deva-gopalani <dmgopalani9@gmail.com>
2025-02-19 15:45:22 +0530

    added TODO which got missed while merging

--- bolna/agent_manager/task_manager.py
+++ bolna/agent_manager/task_manager.py
@@ -903,6 +903,7 @@ class TaskManager(BaseManager):
             logger.info(f"Response from the server {self.webhook_response}")
         else:
             message = format_messages(self.input_parameters["messages"])  # Remove the initial system prompt
+            # TODO revisit this
             self.history.append({
                 'role': 'user',
                 'content': message
