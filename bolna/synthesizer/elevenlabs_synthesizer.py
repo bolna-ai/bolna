7d89a11ba712e126a79f5492d0137fce1a1fcc27
deva-gopalani <dmgopalani9@gmail.com>
2025-02-27 15:58:51 +0530

    frequency reverted for web call

--- bolna/synthesizer/elevenlabs_synthesizer.py
+++ bolna/synthesizer/elevenlabs_synthesizer.py
@@ -46,7 +46,7 @@ class ElevenlabsSynthesizer(BaseSynthesizer):
         self.sender_task = None
         self.conversation_ended = False
         self.current_text = ""
-        self.slicing_range = 4000 if self.use_mulaw else 8000
+        self.slicing_range = 4000 if self.use_mulaw else 16000
 
     # Ensuring we only do wav output for now
     def get_format(self, format, sampling_rate):
