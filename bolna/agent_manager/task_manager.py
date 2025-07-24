9efbb7db10a4c0294566fd2c47137291d1336ec6
Prateek Sachan <ps@prateeksachan.com>
2025-07-24 14:08:55 -0700

    starting tasks in parallel

--- bolna/agent_manager/task_manager.py
+++ bolna/agent_manager/task_manager.py
@@ -378,12 +378,12 @@ class TaskManager(BaseManager):
                     self.filler_preset_directory = f"{os.getenv('FILLERS_PRESETS_DIR')}/{self.synthesizer_voice.lower()}"
 
         # setting transcriber and synthesizer in parallel
-        self.__setup_transcriber()
-        self.__setup_synthesizer(self.llm_config)
-        if not self.turn_based_conversation:
-            self.synthesizer_monitor_task = asyncio.create_task(self.tools['synthesizer'].monitor_connection())
+        # self.__setup_transcriber()
+        # self.__setup_synthesizer(self.llm_config)
+        # if not self.turn_based_conversation:
+        #     self.synthesizer_monitor_task = asyncio.create_task(self.tools['synthesizer'].monitor_connection())
 
-        # asyncio.create_task(self.__async_setup_tools())
+        asyncio.create_task(self.__async_setup_tools())
         # # setting llm
         # llm = self.__setup_llm(self.llm_config)
         # # Setup tasks
@@ -607,7 +607,7 @@ class TaskManager(BaseManager):
 
         return
 
-    def __setup_transcriber(self):
+    async def __setup_transcriber(self):
         try:
             if self.task_config["tools_config"]["transcriber"] is not None:
                 self.language = self.task_config["tools_config"]["transcriber"].get('language', DEFAULT_LANGUAGE_CODE)
@@ -636,7 +636,7 @@ class TaskManager(BaseManager):
         except Exception as e:
             logger.error(f"Something went wrong with starting transcriber {e}")
 
-    def __setup_synthesizer(self, llm_config=None):
+    async def __setup_synthesizer(self, llm_config=None):
         if self._is_conversation_task():
             self.kwargs["use_turbo"] = self.task_config["tools_config"]["transcriber"]["language"] == DEFAULT_LANGUAGE_CODE
         if self.task_config["tools_config"]["synthesizer"] is not None:
@@ -666,6 +666,9 @@ class TaskManager(BaseManager):
                 self.__setup_transcriber(),
                 self.__setup_synthesizer(self.llm_config)
             )
+            if not self.turn_based_conversation:
+                self.synthesizer_monitor_task = asyncio.create_task(self.tools['synthesizer'].monitor_connection())
+
             logger.info("Parallel setup of transcriber and synthesizer completed")
         except Exception as e:
             logger.error(f"Error during parallel setup of tools: {e}")
