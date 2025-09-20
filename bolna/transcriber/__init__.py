230c97fe20e4b3273f0b096e6df360c0f53a7a28
san0808 <sanketbhat882002@gmail.com>
2025-09-20 12:32:19 +0530

    feat: Add SarvamTranscriber support

--- bolna/transcriber/__init__.py
+++ bolna/transcriber/__init__.py
@@ -1,3 +1,4 @@
 from .base_transcriber import BaseTranscriber
 from .deepgram_transcriber import DeepgramTranscriber
 from .azure_transcriber import AzureTranscriber
+from .sarvam_transcriber import SarvamTranscriber
