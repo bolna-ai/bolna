5b3c60ff96eb054d3242081885c6aaacac17ed75
san0808 <sanketbhat882002@gmail.com>
2025-12-18 09:20:16 +0530

    feat: add user online check message to conversation transcript

--- bolna/agent_manager/task_manager.py
+++ bolna/agent_manager/task_manager.py
@@ -2149,6 +2149,7 @@ class TaskManager(BaseManager):
                     else:
                         meta_info={'io': self.tools["output"].get_provider(), "request_id": str(uuid.uuid4()), "cached": False, "sequence_id": -1, 'format': 'pcm', "message_category": "is_user_online_message", 'end_of_llm_stream': True}
                         await self._synthesize(create_ws_data_packet(self.check_user_online_message, meta_info= meta_info))
+                    self.history.append({'role': 'assistant', 'content': self.check_user_online_message})
 
                 # Just in case we need to clear messages sent before
                 await self.tools["output"].handle_interruption()
