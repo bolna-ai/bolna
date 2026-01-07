92b503dbf5140e7180c484015f9bc5b4830c67d3
Aman <amansrivstv@gmail.com>
2026-01-07 14:45:24 +0530

    Added Vobiz telephony handlers

--- bolna/models.py
+++ bolna/models.py
@@ -125,7 +125,7 @@ class IOModel(BaseModel):
 
     @field_validator("provider")
     def validate_provider(cls, value):
-        return validate_attribute(value, ["twilio", "default", "database", "exotel", "plivo"])
+        return validate_attribute(value, ["twilio", "default", "database", "exotel", "plivo", "vobiz"])
 
 
 # Can be used to route across multiple prompts as well
